e7294dd950be3953f617f5ef9e7b5920e2b3271d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-12 23:21:47 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
+# connect up all the files in our application to the appropriate gems and to each other.
+# This environment.rb file loads Bundler and thus all the gems in our Gemfile, as well as the app directory.
+
+require_relative 'config/environment' 
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye 
+	end
+
+	get '/date' do
+		erb :date
 	end
 
 
